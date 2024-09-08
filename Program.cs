b64d26dba95621f572eb14417f9eae29b8117aec
Ali Asghar Gishkori <gishkori@hotmail.com>
2024-09-08 17:17:29 +1000

    Create a project for the leet code problem 121

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Cryptography;
+
+public class Solution
+{
+    public int MaxProfit(int[] prices)
+    {
+        return 0;
+    }
+}
+
+
+class Program
+{
+    static void Main()
+    {
+        Solution profit = new Solution();
+        Console.WriteLine(profit.MaxProfit([1, 3]));
+
+    }
+}
+
