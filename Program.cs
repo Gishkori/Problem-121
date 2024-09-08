fbe941cb3041fba61059893843cc46b0a857bbe2
Ali Asghar Gishkori <gishkori@hotmail.com>
2024-09-08 19:39:22 +1000

    Added function to return the max profit for the prices which are provided

--- Program.cs
+++ Program.cs
@@ -2,8 +2,30 @@
 {
     public int MaxProfit(int[] prices)
     {
+        int buyPrice = prices[0];
+        int index = 0;
+        for (int i = 0; i < prices.Length; i++)
+        {
+            if (prices[i] < buyPrice)
+            {
+                buyPrice = prices[i];
+                index = i;
+            }
 
-        return 0;
+
+        }
+
+        int sellPrice = 0;
+        for (int j = index + 1; j < prices.Length; j++)
+        {
+            int tempSell = prices[j] - buyPrice;
+            if (tempSell > sellPrice)
+            {
+                sellPrice = tempSell;
+            }
+        }
+        Console.WriteLine(index + " " + buyPrice + " " + sellPrice);
+        return sellPrice;
     }
 }
 
@@ -13,7 +35,7 @@ class Program
     static void Main()
     {
         Solution profit = new Solution();
-        Console.WriteLine(profit.MaxProfit([1, 3]));
+        Console.WriteLine(profit.MaxProfit([2, 4, 1]));
 
     }
 }
--- obj/Debug/net8.0/Problem-121.AssemblyInfo.cs
+++ obj/Debug/net8.0/Problem-121.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Problem-121")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94315c31b121b86fe4b87493ea7347c471b8bed1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+828457e7564d3219fd07d553346f1de997d92f4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Problem-121")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Problem-121")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
